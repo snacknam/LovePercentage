08a467e6c10dc7c970ea934e4e73638d6f64c56d
snack <kwansiknam@gmail.com>
2025-02-01 19:15:35 +0900

    add male/female data

---
+++ script.js
@@ -0,0 +1,33 @@
+const maleAgeGroups = [
+    { min: 20, max: 24, count: 1529757 },
+    { min: 25, max: 29, count: 1907333 },
+    { min: 30, max: 34, count: 1905868 },
+    { min: 35, max: 39, count: 1745532 },
+    { min: 40, max: 44, count: 2097082 },
+    { min: 45, max: 49, count: 1981666 },
+    { min: 50, max: 54, count: 2262196 },
+    { min: 55, max: 59, count: 2073954 },
+    { min: 60, max: 64, count: 2098812 },
+    { min: 65, max: 69, count: 1632277 },
+    { min: 70, max: 74, count: 1054423 },
+    { min: 75, max: 79, count: 740365 },
+];
+const maleTotal = 21029265;
+
+const femaleAgeGroups = [
+    { min: 20, max: 24, count: 1385901 },
+    { min: 25, max: 29, count: 1672602 },
+    { min: 30, max: 34, count: 1652002 },
+    { min: 35, max: 39, count: 1588375 },
+    { min: 40, max: 44, count: 1986090 },
+    { min: 45, max: 49, count: 1912828 },
+    { min: 50, max: 54, count: 2220265 },
+    { min: 55, max: 59, count: 2079120 },
+    { min: 60, max: 64, count: 2148392 },
+    { min: 65, max: 69, count: 1735005 },
+    { min: 70, max: 74, count: 1198502 },
+    { min: 75, max: 79, count: 930581 },
+];
+const femaleTotal = 20509663;
+
+const overallTotal = maleTotal + femaleTotal;
